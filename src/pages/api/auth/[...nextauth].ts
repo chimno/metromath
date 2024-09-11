be1d5b6becff5605fe433c940a3376e82a8d2320
chimno <clash9989@gmail.com>
2024-09-11 09:40:59 +0900

    fix: vercel error 4

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,48 +1,30 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import clientPromise from "../../../utils/database";
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        if (!credentials) return null;
-
-        const client = await clientPromise;
-        const db = client.db('matrix_math_game');
-        const usersCollection = db.collection('users');
-
-        const user = await usersCollection.findOne({ email: credentials.email });
-        if (!user) return null;
-
-        const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
-        if (!isPasswordValid) return null;
-
-        return { id: user._id.toString(), name: user.nickname, email: user.email };
-      }
-    })
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
   ],
-  session: {
-    strategy: "jwt"
-  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.id = user.id;
+        token.id = user.id
       }
-      return token;
+      return token
     },
-    async session({ session, token }) {
-      if (token) {
-        session.user.id = token.id;
+    async session({ session, token, user }) {
+      if (session.user) {
+        session.user.id = token.id as string
       }
-      return session;
+      return session
     },
   },
-});
+}
+
+export default NextAuth(authOptions)
